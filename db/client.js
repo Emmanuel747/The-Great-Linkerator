cd2fddf1f1ccbd0e04b992830d4ead2ef199c443
Emmanuel747 <EmmanuelEEjim@gmail.com>
2021-06-12 11:14:27 -0500

    DB Only

--- db/client.js
+++ db/client.js
@@ -3,17 +3,16 @@ require('dotenv').config()
 const { Client } = require("pg");
 const DB_NAME = "localhost:5432/linkerator";
 const DB_URL = process.env.DATABASE_URL || `postgres://${DB_NAME}`;
-// const client = new Client(DB_URL);
-
-const { FITDEV_DB_PASSWORD, FITDEV_DB_USERNAME } = process.env;
+const client = new Client(DB_URL);
 
 //For Eman
-const client = new Client({
-   connectionString:
-     // `postgres://localhost:5432/fitness-dev`
-     // I need the login credentials to access postgres on my
-     process.env.DATABASE_URL || `postgres://${FITDEV_DB_USERNAME}:${FITDEV_DB_PASSWORD}@localhost:5432/fitness-dev`,
- });
+// const { FITDEV_DB_PASSWORD, FITDEV_DB_USERNAME } = process.env;
+// const client = new Client({
+//    connectionString:
+//      // `postgres://localhost:5432/fitness-dev`
+//      // I need the login credentials to access postgres on my
+//      process.env.DATABASE_URL || `postgres://${FITDEV_DB_USERNAME}:${FITDEV_DB_PASSWORD}@localhost:5432/fitness-dev`,
+//  });
 
 
 module.exports = client;
